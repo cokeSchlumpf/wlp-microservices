3a02e3188488b3c68ad6194f2df27aa576126b56
Michael Wellner <michael.wellner@de.ibm.com>
2015-11-03 08:09:49 +0100

    Sample Logging Usecase.

--- ws-tradeprocessing/src/main/java/camel/SampleRoute.java
+++ ws-tradeprocessing/src/main/java/camel/SampleRoute.java
@@ -5,6 +5,7 @@ import org.apache.camel.model.dataformat.JsonLibrary;
 
 import camel.beans.PrepareLogging;
 import camel.beans.PrepareRest;
+import ws.InternalLogs;
 
 public class SampleRoute extends RouteBuilder {
 
@@ -12,10 +13,20 @@ public class SampleRoute extends RouteBuilder {
 	public void configure() throws Exception {
 		from("direct:hello") //
 				.bean(PrepareLogging.class, "info") //
-				.bean(PrepareRest.class) // 
+				.multicast().parallelProcessing().to("direct:callBean", "direct:callHttp");
+
+		from("direct:callBean") //
+				.bean(InternalLogs.class, "create") //
+				.to("log:SampleRoute[callBean]") //
+				.bean(InternalLogs.class, "list") //
+				.to("log:SampleRoute[callBean]");
+
+		from("direct:callHttp") //
+				.bean(PrepareRest.class) //
 				.marshal().json(JsonLibrary.Jackson) //
-				.to("log:SampleRoute?showAll=true")
-				.to("http://localhost:9080/ws-logging/api/logs");
+				.to("log:SampleRoute[callHttp]") //
+				.to("http://localhost:9080/ws-logging/api/logs") //
+				.to("log:SampleRoute[callHttp]");
 	}
 
 }
---
+++ ws-tradeprocessing/src/main/java/ws/InternalLogs.java
@@ -0,0 +1,50 @@
+package ws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import api.LoggingService;
+import model.logging.LogEntry;
+import model.logging.LogLevel;
+
+@Stateless
+@Path("logs")
+public class InternalLogs implements LoggingService {
+
+	private static final List<LogEntry> log = new ArrayList<>();
+
+	@POST
+	@Override
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public LogEntry create(LogEntry entry) {
+		LogEntry result = new LogEntry(Long.valueOf(log.size() + 1), entry.getLevel(), entry.getMessage());
+		log.add(result);
+		return result;
+	}
+
+	@GET
+	@Path("example")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.WILDCARD)
+	public LogEntry example() {
+		return new LogEntry(LogLevel.INFO, "Lorem ipsum dolor sit amet del arem.");
+	}
+
+	@GET
+	@Override
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.WILDCARD)
+	public List<LogEntry> list() {
+		return log;
+	}
+
+}
